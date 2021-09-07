6ad60e739bd52ef496073d530a301ffcca3ae9f1
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-09-07 17:35:18 +0500

    update and delete post

--- Routes/posts.js
+++ Routes/posts.js
@@ -12,7 +12,33 @@ router.post("/", async (req, res) => {
   }
 });
 //update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("post has been updated");
+    } else {
+      res.status(403).json("you can update only your own post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //like a post
 //get a post
 //get timeline posts
